b9491a707260945671fd1a57614ca3db1f460b18
Shivang Yadav <60583562+shivang-9357@users.noreply.github.com>
2021-08-14 03:54:14 +0530

    Update fire.js

--- src/fire.js
+++ src/fire.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAbGO5eCP8CbNeuUoT-TMptFB7upz6lSWw",
-    authDomain: "weight-tracker-9c5f2.firebaseapp.com",
-    projectId: "weight-tracker-9c5f2",
-    storageBucket: "weight-tracker-9c5f2.appspot.com",
-    messagingSenderId: "617443987343",
-    appId: "1:617443987343:web:04b0f45f69e48e1379c5fa"
+    //your firebase credentials
   };
 
 const fire = firebase.initializeApp(firebaseConfig);
